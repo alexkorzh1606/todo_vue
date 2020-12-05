2b9be46a60de8438934eb89508c7d3df6535ff8f
Alex Korzh <alex.korzh@hotmail.com>
2020-12-05 22:14:33 +0300

    todo item added

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,60 @@
 <template>
   <v-app>
     <v-main>
-      <TodoList/>
+        <v-container>
+    <v-row class="text-center">
+      <v-col>
+        <h1>ДЕЛА</h1>
+        <v-input>
+          <v-col>
+            <v-text-field v-model="message" label="Введите ваше дело сюда" required></v-text-field>
+            <v-btn color="success" class="mr-4" block @click="addToList">Добавить новое дело</v-btn>
+          </v-col>
+        </v-input>
+        <h4 class='title is-4'>Ваши дела</h4>
+      </v-col>
+    </v-row>
+  </v-container>
+      <!-- <TodoList v-bind:message="message"
+                v-on:add="addToList()"/> -->
+      <TodoItem v-for="(item, index) in list"
+                v-bind:key="index"
+                v-bind:title="item"
+                v-on:delete="deleteFromList(index)"
+      />
     </v-main>
   </v-app>
 </template>
 
 <script>
-import TodoList from './components/TodoList.vue';
+// import TodoList from './components/TodoList.vue';
+import TodoItem from './components/TodoItem.vue';
 
 export default {
   name: 'App',
 
   components: {
-    TodoList,
+    // TodoList,
+    TodoItem,
   },
 
-  data: () => ({
-    //
-  }),
+  data() {
+    return {
+      message: 'Купить крекер',
+      list: [
+        'Найти кота',
+        'починить',
+      ],
+    };
+  },
+
+  methods: {
+    deleteFromList(e) {
+      this.list.splice(e, 1);
+    },
+    addToList() {
+      this.list.push(this.message);
+    },
+  },
 };
 </script>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <v-card class="mx-auto" max-width="400" tile>
+          <v-list-item two-line>
+            <v-list-item-content>
+              <v-list-item-title>
+                <p class="text-center">{{title}}</p>
+              </v-list-item-title>
+              <v-list-item-subtitle class="text-center">
+                <v-btn color="error" class="mr-4" @click="deleteItem">Удалить дело</v-btn>
+              </v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'TodoItem',
+  props: {
+    title: String,
+  },
+  methods: {
+    deleteItem() {
+      this.$emit('delete');
+    },
+  },
+};
+</script>
